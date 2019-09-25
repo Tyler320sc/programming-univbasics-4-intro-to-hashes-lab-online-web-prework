341cb7ade13a1636b7bcccf104f261017a361263
Tyler Carroll <tylercarroll1611@gmail.com>
2019-09-25 00:36:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,13 +48,13 @@ end
 
  def update_counting_hash(hash, key)
    {
-     hash[key]  
+     key  
    }
    
    
    
    
-#   if key # or maybe first access the hash and then [key]?
+#   if key    # or maybe first access the hash and then [key]?
 #     key += 1
 #   else
 #     key = 1
